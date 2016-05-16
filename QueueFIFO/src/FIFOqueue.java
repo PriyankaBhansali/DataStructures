dde1cbc114582c87332124b0a76f696b5e51cf87
Priyanka Bhansali <piyubhansali@gmail.com>
2016-05-15 21:30:45 -0700

    commit

--- QueueFIFO/src/FIFOqueue.java
+++ QueueFIFO/src/FIFOqueue.java
@@ -25,8 +25,8 @@ class Queue {
 		throw new Exception("Queue is Full");
 		else if(isEmpty()){
 			front++;
-                    rear++;
-                    a[rear] = v;
+                        rear++;
+                        a[rear] = v;
 		}
 		else
 		{
@@ -86,8 +86,6 @@ class Demo {
         q.add(80);
         System.out.println( q.remove());
         System.out.println( q.toString());
-
-
     }
 
 }
